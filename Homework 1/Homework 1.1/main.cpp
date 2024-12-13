d662fc8fc79e1cce8a93360d0c1f738ca1f550ec
Mybro007 <Mybro007@mail.ru>
2024-12-13 20:32:07 +0300

    Homework 1 v2.0

--- Homework 1/Homework 1.1/main.cpp
+++ Homework 1/Homework 1.1/main.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
+#include <chrono>
 #include<thread>
 
 void foo1(int& counter, int maxClient)
 {
-	if (counter < maxClient)
+	while (counter <= maxClient)
 	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 		counter++;
 	}
 }
 
 void foo2(int& counter)
 {
-	if (counter > 0)
+	while (counter != 0)
 	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 		counter--;
 	}
 }
@@ -24,8 +27,8 @@ int main()
 	std::cout << "Input the MAX quantity of clients: ";
 	std::cin >> maxClient;
 	std::thread cl(foo1, std::ref(counter), maxClient);
-	cl.detach();
 	std::thread op(foo2, std::ref(counter));
+	cl.detach();
 	op.join();
 	return 0;
 }
--- Homework 1/Homework 1.2/main.cpp
+++ Homework 1/Homework 1.2/main.cpp
@@ -3,31 +3,87 @@
 #include<Windows.h>
 #include<vector>
 
-void f1(int numCPU);
-void f2();
-void f4();
-void f8();
-void f16();
-
-void foo1();
-void foo2();
-void foo3();
-void foo4();
+void foo1(int qth);
+void foo2(int qth);
+void foo3(int qth);
+void foo4(int qth);
 
 int main()
 {
 	SYSTEM_INFO sysInfo;
 	GetSystemInfo(&sysInfo);
 	int numCPU = sysInfo.dwNumberOfProcessors;
-	std::thread t1(f1, numCPU);
-	t1.join();
+	int qth = 1;
+	while (qth <= 16)
+	{
+		if (qth == 1)
+		{
+			std::cout << "Number of hardware cores: " << numCPU << "\n\n";
+			std::cout << "\t   1000\t\t10000\t\t100000\t\t1000000\n";
+		}
+		std::cout << qth << " streams: ";
+		
+		std::vector<std::thread> threads(qth);
+		for (int j = 0; j < 4; j++)
+		{
+			if (j == 0)
+			{
+				auto start = std::chrono::steady_clock::now();
+				for (int i = 0; i < threads.size(); i++)
+				{
+					threads[i] = std::thread(foo1, qth);
+					threads[i].join();
+				}
+				auto end = std::chrono::steady_clock::now();
+				std::chrono::duration<double> elapsedSeconds = end - start;
+				std::cout << elapsedSeconds.count() << "\t";
+			}
+			else if (j == 1)
+			{
+				auto start = std::chrono::steady_clock::now();
+				for (int i = 0; i < threads.size(); i++)
+				{
+					threads[i] = std::thread(foo2, qth);
+					threads[i].join();
+				}
+				auto end = std::chrono::steady_clock::now();
+				std::chrono::duration<double> elapsedSeconds = end - start;
+				std::cout << elapsedSeconds.count() << "\t";
+			}
+			else if (j == 2)
+			{
+				auto start = std::chrono::steady_clock::now();
+				for (int i = 0; i < threads.size(); i++)
+				{
+					threads[i] = std::thread(foo3, qth);
+					threads[i].join();
+				}
+				auto end = std::chrono::steady_clock::now();
+				std::chrono::duration<double> elapsedSeconds = end - start;
+				std::cout << elapsedSeconds.count() << "\t";
+			}
+			else
+			{
+				auto start = std::chrono::steady_clock::now();
+				for (int i = 0; i < threads.size(); i++)
+				{
+					threads[i] = std::thread(foo4, qth);
+					threads[i].join();
+				}
+				auto end = std::chrono::steady_clock::now();
+				std::chrono::duration<double> elapsedSeconds = end - start;
+				std::cout << elapsedSeconds.count() << "\t" << "\n";
+			}
+		}
+		qth *= 2;
+	}
 	
 	return 0;
 }
 
-void foo1()
+void foo1(int qth)
 {
-	int size = 1000;
+	int size = 1000/qth;
 	std::vector<int> v1(size);
 	std::vector<int> v2(size);
 	for (int i = 0; i < size; i++)
@@ -36,9 +92,9 @@ void foo1()
 	}
 }
 
-void foo2()
+void foo2(int qth)
 {
-	int size = 10000;
+	int size = 10000/qth;
 	std::vector<int> v1(size);
 	std::vector<int> v2(size);
 	for (int i = 0; i < size; i++)
@@ -47,9 +103,9 @@ void foo2()
 	}
 }
 
-void foo3()
+void foo3(int qth)
 {
-	int size = 100000;
+	int size = 100000/qth;
 	std::vector<int> v1(size);
 	std::vector<int> v2(size);
 	for (int i = 0; i < size; i++)
@@ -58,54 +114,13 @@ void foo3()
 	}
 }
 
-void foo4()
+void foo4(int qth)
 {
-	int size = 100000;
+	int size = 100000/qth;
 	std::vector<int> v1(size);
 	std::vector<int> v2(size);
 	for (int i = 0; i < size; i++)
 	{
 		v1[i] += v2[i];
 	}
-}
-
-void f1(int numCPU)
-{
-	std::cout << "Number of hardware cores: " << numCPU << "\n\n";
-	std::cout << "\t   1000\t\t10000\t\t100000\t\t1000000\n";
-
-	std::cout << "1 streams: ";
-	auto start = std::chrono::steady_clock::now();
-	foo1();
-	auto end = std::chrono::steady_clock::now();
-	std::chrono::duration<double> elapsedSeconds = end - start;
-	std::cout << elapsedSeconds.count() << "\t";
-
-	start = std::chrono::steady_clock::now();
-	foo2();
-	end = std::chrono::steady_clock::now();
-	elapsedSeconds = end - start;
-	std::cout << elapsedSeconds.count() << "\t";
-
-	start = std::chrono::steady_clock::now();
-	foo3();
-	end = std::chrono::steady_clock::now();
-	elapsedSeconds = end - start;
-	std::cout << elapsedSeconds.count() << "\t";
-
-	start = std::chrono::steady_clock::now();
-	foo4();
-	end = std::chrono::steady_clock::now();
-	elapsedSeconds = end - start;
-	std::cout << elapsedSeconds.count() << "\t";
-}
-
-void oneThread()
-{
-
-}
-
-void f2()
-{
-
 }
